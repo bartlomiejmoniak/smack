e9e9d32c91ab8c8b60687ff9e4b994ac65f8cb2e
Bartłomiej Moniak <bm@Bartomiejs-MacBook-Pro.local>
2017-08-17 18:06:34 +0200

    End of lesson 2

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChanellVC.swift
+//  Smack
+//
+//  Created by Bartłomiej Moniak on 17/08/2017.
+//  Copyright © 2017 Bartłomiej Moniak. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
